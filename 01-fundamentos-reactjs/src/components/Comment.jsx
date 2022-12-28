a9d4aeff9b07e2562aacae9f25f52e2ed2288247
verdeli <jonathanverdeli@gmail.com>
2022-12-28 16:53:15 -0300

    feat: implementação comentários estaticos

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import styles from './Comment.module.css'
+import {Trash, ThumbsUp } from 'phosphor-react';
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <img src='https:;;github.com/verdeli.png' />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Verdelin</strong>
+                            <time dateTime='2022-28-12 14:59:33'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar Comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Vewrdelin!</p>
+                </div>
+                <footer>
+                   <button>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                   </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
