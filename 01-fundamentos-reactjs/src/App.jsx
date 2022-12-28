f8ac52826cb77328f93b8c5b77479f1cadab9e08
verdeli <jonathanverdeli@gmail.com>
2022-12-28 14:14:49 -0300

    feat: Create  Vite + React

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post";
+
+function App() {
+return(
+  <div>
+  <h1>Hello World</h1>
+  <Post author="Leo" content="Vamos camarada somente dê a ordem!" />
+  <Post author="Verdelin" content="Comunada tá em peso!" />
+  </div>
+);
+}
+
+export default App
---
+++ 01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props){
+
+    return(
+        <div>
+            <p>{props.author} - {props.content}</p>
+        </div>
+    ) 
+}
